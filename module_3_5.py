1d76773ef9c8e4a3e8f6294debe4c54974ae0b6a
panov-o <panov_o@mail.ru>
2024-09-26 17:40:43 +0700

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -3,8 +3,10 @@ def get_multiplied_digits(number):
     first = int(str_number[0])
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
-    else:
+    elif first > 0:
         return first
+    else:
+        return 1
 
 result = get_multiplied_digits(40203)
 print(result)
