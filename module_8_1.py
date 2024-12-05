4565d06b1c4a7ce74ceece025672fcd4c0cb6b42
panov-o <panov_o@mail.ru>
2024-12-05 15:17:29 +0700

    Update module_8_1.py

--- module_8_1.py
+++ module_8_1.py
@@ -2,10 +2,7 @@ def add_everything_up(a, b):
     try:
         return a + b
     except:
-        if isinstance(a,(int, float)) and isinstance(b, str):
-            return str(a) + b
-        else:
-            return a + str(b)
+        return str(a) + str(b)
 
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
