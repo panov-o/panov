7b11388fa0fc0e878742790b613cefa1001997cd
panov-o <panov_o@mail.ru>
2024-12-15 11:43:27 +0700

    Update module_9_4.py

--- module_9_4.py
+++ module_9_4.py
@@ -12,3 +12,18 @@ def get_advanced_writer(file_name):
 
 write = get_advanced_writer('example.txt')
 write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+from random import choice
+
+class MysticBall:
+    def __init__(self, *words):
+        self.words = words
+
+    def __call__(self):
+        return choice(self.words)
+
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())
